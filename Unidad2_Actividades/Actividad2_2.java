85aa343b3bfeaab3b02ae76c94d0cd6e21d4c2f2
PROGRAMACION <albasanchezolmedo00@gmail.com>
2024-01-23 17:11:24 +0100

    ActividadesU1

--- Unidad2_Actividades/Actividad2_2.java
+++ Unidad2_Actividades/Actividad2_2.java
@@ -2,17 +2,16 @@ package Unidad2_Actividades;
 
 import java.util.Scanner;
 
-public class Actividad_2_1 {
+public class Actividad2_2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int num;
         System.out.println("Introduce un número: ");
         num = sc.nextInt();
-
-        if (num % 2 == 0) {
-            System.out.println("El número es par");
+        if (num > 0) {
+            System.out.println("El número es positivo");
         } else {
-            System.out.println("El número es impar");
+            System.out.println("El número es negativo");
         }
     }
 }
