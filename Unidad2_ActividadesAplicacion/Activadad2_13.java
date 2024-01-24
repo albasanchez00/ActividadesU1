997b02dac4324de36b96f3fdcaf1b8524d07844b
PROGRAMACION <albasanchezolmedo00@gmail.com>
2024-01-24 20:08:15 +0100

    ActividadesU2

---
+++ Unidad2_ActividadesAplicacion/Activadad2_13.java
@@ -0,0 +1,17 @@
+package Unidad2_ActividadesAplicacion;
+
+import java.util.Scanner;
+
+public class Activadad2_13 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int numAnimales,comidaDiaria,kgAnimal;
+        System.out.println("Introduce el número de animales: ");
+        numAnimales=sc.nextInt();
+        System.out.println("Introduce la cantidad de comida diaria: ");
+        comidaDiaria=sc.nextInt();
+        System.out.println("Introduce la media de kg come cada animal: ");
+        kgAnimal=sc.nextInt();
+
+    }
+}
--- Unidad2_ActividadesAplicacion/Actividad1_12.java
+++ Unidad2_ActividadesAplicacion/Actividad1_12.java
@@ -5,6 +5,35 @@ import java.util.Scanner;
 public class Actividad1_12 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int DNI
+        int numDni,calculo;
+        char letra=' ';
+        System.out.println("Introduce el número del DNI (sin letra): ");
+        numDni=sc.nextInt();
+        calculo=numDni%23; //0 al 22
+        switch (calculo){
+            case 1->letra='T';
+            case 2->letra='R';
+            case 3->letra='W';
+            case 4->letra='A';
+            case 5->letra='G';
+            case 6->letra='M';
+            case 7->letra='Y';
+            case 8->letra='F';
+            case 9->letra='P';
+            case 10->letra='D';
+            case 11->letra='X';
+            case 12->letra='B';
+            case 13->letra='J';
+            case 14->letra='Z';
+            case 15->letra='S';
+            case 16->letra='Q';
+            case 17->letra='V';
+            case 18->letra='H';
+            case 19->letra='L';
+            case 20->letra='C';
+            case 21->letra='K';
+            case 22->letra='E';
+        }
+        System.out.println("La letra del DNI " + numDni + " es " + letra);
     }
 }
