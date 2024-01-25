f5057cac818e8e3f6f76193df43b582caf8276d3
PROGRAMACION <albasanchezolmedo00@gmail.com>
2024-01-25 20:55:04 +0100

    ActividadesU2

--- Unidad2/Unidad2_ActividadesFinales/Actividad2_19.java
+++ Unidad2/Unidad2_ActividadesFinales/Actividad2_19.java
@@ -1,10 +1,23 @@
 package Unidad2.Unidad2_ActividadesFinales;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Actividad2_19 {
     public static void main(String[] args) {
+        //Indicar los grados y calcular los radianes.
+        // radianes = grados*pi/180;
+        //grados%=360
         Scanner sc=new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        double grados=0;
+        System.out.println("Introduce los grados del angulo-> ");
+        grados= sc.nextDouble();
+        grados%=360;
+        double radianes=grados*Math.PI/180;
+        System.out.print("Los radianes son: "); //no hace salto de linea
 
+        //Redondear a cuatro decimales:
+        System.err.printf("%.4f %n",radianes);
     }
 }
