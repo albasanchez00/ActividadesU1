5791ac888b20399bc705052f161713fa2ff2efc8
PROGRAMACION <albasanchezolmedo00@gmail.com>
2024-01-31 17:11:07 +0100

    ActividadesU2

--- Unidad3/Actividades_Resueltas/Actividad3_14.java
+++ Unidad3/Actividades_Resueltas/Actividad3_14.java
@@ -5,5 +5,12 @@ import java.util.Scanner;
 public class Actividad3_14 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("**********TABLA  DE MULTIPLICAR***********");
+        for (byte i=1;i<=10;i++){
+            System.out.println("**TABLA DEL " + i +"**");
+            for (byte y=1;y<=10;y++){
+                System.out.println(i + " x " + y + " = " + (i*y));
+            }
+        }
     }
 }
