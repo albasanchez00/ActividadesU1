55f4f2333f1c63709f8a6f6f2687fc61e398709c
PROGRAMACION <albasanchezolmedo00@gmail.com>
2024-02-22 19:29:03 +0100

    ActividadesU2

---
+++ Repaso/Ejemplo1.java
@@ -0,0 +1,50 @@
+package Repaso;
+
+import java.util.Scanner;
+
+public class Ejemplo1 {
+    public static void main(String[] args) {
+        //Declaramos nuestras variables
+        int habitantes,total=0;
+
+        String nombreUsu;
+        String poblacion;
+
+
+        /*
+        Luego tenemos las declaraciones de las clase Scanner que nos permite leer la informacion
+        que solicitamos, siempre relacionado con el tipo de dato que se va a escribir
+         */
+        //Declaracion
+        Scanner sc=new Scanner(System.in);
+        //Ej: Solicita el nimbre, la poblacion y numero de habitantes que sean las 5 capitales de provincias CLM
+        //Sumar el total de habitantes de las 5 capittales
+
+
+        //Condicionales
+
+
+        //Bucles
+        //Utilizamos un bucle desde 1 hasta 5 y lee lkos datos.
+        for (int i=1;i<=5;i++){
+            System.out.println("Indica la Provincia -> ");
+            poblacion= sc.nextLine();
+            System.out.println("Indica el número de habitantes -> ");
+            habitantes= sc.nextInt();sc.nextLine();
+            System.out.println("Indica tu nombre -> ");
+            nombreUsu= sc.nextLine();
+            /*
+            Condicional -> si el numero de habitantes es menor que 20.000, indicar que hay un error y que restar 1 a i
+            para que vuelva a solicitar la informacion.
+             */
+            if (habitantes<20000){
+                i--;
+                System.out.println("Los habitantes de " + poblacion + "son más de " + habitantes + ". Vuelve  a insertar la información.");
+            } else {
+                total+=habitantes;
+            }
+
+        }
+        System.out.println("El número de habitantes de las Provincias de CLM es " + total);
+    }
+}
---
+++ Repaso/Ejemplo_Comandas.java
@@ -0,0 +1,86 @@
+package Repaso;
+
+import java.util.Scanner;
+
+/*
+Enunciado
+*Crear un programa que permita a traves de un menu
+(toritlla,croquetas,jamon y queso. bebidas)
+*Indicar lo que el cliente solicita, y  crear o mostrar el toal de su pedido,
+tomando el cuenta, los precios
+-Toritlla:2€ string
+-Croquetas:8€
+-Jamon y queso:15€ string
+-Bebidas:3€
+ */
+public class Ejemplo_Comandas {
+    public static void main(String[] args) {
+        //Declaraciones de variables
+        Scanner sc=new Scanner(System.in);
+        int cuenta=0;
+        int cant=0;
+        double total=0;
+        double iva=0.21;
+        String detallePedido="";
+        byte opcion=0;
+        /*
+        * Crear un bucle con el menú
+        */
+        System.out.println("·····Menú Cofee House·····");
+        System.out.println("\t1. Torillas");
+        System.out.println("\t2. Croquetas");
+        System.out.println("\t3. Jamón y queso");
+        System.out.println("\t4. Bebidas");
+        System.out.println("\t5. Salir");
+        do {
+
+            System.out.print("\n\t Indique su opción -> ");
+            opcion= sc.nextByte();
+            switch (opcion){
+                case 1-> {
+                    System.out.print("\t Inserta la cantidad -> ");
+                    cant= sc.nextInt();
+                    cuenta+=(2*cant);
+                    detallePedido+="\t" + cant + "\tTortillas\n";
+                }
+                case 2-> {
+                    System.out.print("\t Inserta la cantidad -> ");
+                    cant= sc.nextInt();
+                    cuenta+=(8*cant);
+                    detallePedido+="\t" + cant + "\tCroquetas\n";
+                }
+                case 3-> {
+                    System.out.print("\t Inserta la cantidad -> ");
+                    cant= sc.nextInt();
+                    cuenta+=(15*cant);
+                    detallePedido+= "\t" + cant + "Jamón y queso\n";
+                }
+                case 4-> {
+                    System.out.print("\t Inserta la cantidad -> ");
+                    cant= sc.nextInt();
+                    cuenta+=(3*cant);
+                    detallePedido+="\t" + cant + "\tBebidas\n";
+                }
+                case 5-> System.out.println("\t·····Generando cuenta·····");
+                default -> {
+                    System.err.println("Opción no valida");
+                }
+            }
+
+        }while (opcion!=5);
+        iva *= cuenta;
+        total = iva + cuenta;
+        System.out.println("\n\n·········Factura Simplificada·········");
+        System.out.println("\tPedido: \n" + detallePedido);
+        System.out.println("\tTotal sin IVA: " + cuenta +"€");
+        System.out.print("\tIVA:");
+        System.out.printf("%15.2f",iva);
+        System.out.println("€\n\tImporte total (con IVA): " + total +"€");
+        System.out.println("······································");
+
+        //double iva=0.21;
+        /*"%15.2f" -> Sirve para mostrar solo dos decimales*/
+        //System.out.printf("%0.00f", iva);
+
+    }
+}
