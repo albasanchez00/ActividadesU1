33a99433ef12dca0132ecc5a32d581b3c79358f8
PROGRAMACION <albasanchezolmedo00@gmail.com>
2024-02-26 19:41:03 +0100

    ActividadesU2

---
+++ Unidad4/Ejemplos/U4_Ejemplo_Return.java
@@ -0,0 +1,23 @@
+package Unidad4.Ejemplos;
+
+import java.util.Scanner;
+
+public class U4_Ejemplo_Return {
+    public static void main(String[] args) {
+        String nombreUsuario=llamadaNombre(3);
+        System.out.print("Tu nombre es -> " + nombreUsuario);
+        int suma= funcionSumar(2,4);
+        System.out.print("\nEl resultado de a + b = " + suma);
+    }
+    public static String llamadaNombre(int contador) {
+        Scanner sc=new Scanner(System.in);
+        String nombre="";
+        System.out.print("Indica el nombre -> ");
+        nombre= sc.nextLine();
+        return nombre;
+    }
+
+    public static int funcionSumar(int a, int b) {
+        return a+b;
+    }
+}
