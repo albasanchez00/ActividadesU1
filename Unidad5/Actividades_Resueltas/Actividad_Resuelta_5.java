d9e16be86fcab92e8889e2384053e8b3c6d71543
PROGRAMACION <albasanchezolmedo00@gmail.com>
2024-03-04 21:24:21 +0100

    ActividadesU2

--- Unidad5/Actividades_Resueltas/Actividad_Resuelta_5.java
+++ Unidad5/Actividades_Resueltas/Actividad_Resuelta_5.java
@@ -1,24 +1,32 @@
 package Unidad5.Actividades_Resueltas;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Actividad_Resuelta_5 {
-    static int[] rellenaPares (int longitud, int fin) {
-        //Creamos la tabla con la longitud adecuada.
-        int[] pares=new int[longitud];
-
-        int i=0; //Indica con que elemento de tabla estamos trabajando.
-
-        while (i<pares.length){
-            //numero de pares sea igual que la longitud de la tabla.
-            int num= (int) (Math.random()*fin+1);
-            if (num%2==0){ //si es par.
-                pares[i]=num; //lo guardamos en el elemento i.
-                i++; //Incrementamos el indicador.
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        mostrar("Indica la longitud de la tabla");
+        int longitud= sc.nextInt();
+        int pares[]=new int[longitud];
+        mostrar("Indica el numero fin de los números aleatorios ->");
+        int fin= sc.nextInt();
+        for (int i=0;i<pares.length;i++){
+            int num=(int) (Math.random()*fin+1);
+            if (num%2==0){
+                pares[i]=num;
+            }else {
+                --i;
             }
         }
-        //Ahora nos falta ordenar la tabla
         Arrays.sort(pares);
-        return (pares);
+        mostrar("Tabla ordenada " + Arrays.toString(pares));
+    }
+
+    public static void mostrar(String texto) {
+        System.out.println("\t"+texto);
+    }
+    public static void mostrarSinLn(String texto) {
+        System.out.print("\t"+texto);
     }
 }
